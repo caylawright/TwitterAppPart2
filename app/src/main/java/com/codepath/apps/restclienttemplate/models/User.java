4e6e144c44317a5170ae52f296515b20daf49872
caylawright <cwrigh27@bulldogs.aamu.edu>
2020-09-28 02:16:22 -0500

    twitter part 2

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,25 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+@Parcel
+public class User {
+
+    //empty constructor needed by the Parceler library
+    public User(){}
+
+    public String name;
+    public String screenName;
+    public String profileImageUrl;
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+        user.name = jsonObject.getString("text");
+        user.screenName = jsonObject.getString("name");
+        user.profileImageUrl = jsonObject.getString("profile_image_url_https");
+        return user;
+
+    }
+}
